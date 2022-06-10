da9b685ebeb57637ab6fa3940e3782b8b3c09f35
NidaGitHub <107262867+NidaGitHub@users.noreply.github.com>
2022-06-10 19:49:21 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import ProjectOne from './ProjectOne'
+function App() {
+  return (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <ProjectOne />
+    </div>
+  );
+}
+export default App;
